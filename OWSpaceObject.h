ad0038797c6e3104ae1b8f0a5899102042beee8d
Jim Veneskey <github@strifecrafter.com>
2015-03-26 12:23:58 -0400

    Adding planetary pictures and a new class

---
+++ OWSpaceObject.h
@@ -0,0 +1,13 @@
+//
+//  OWSpaceObject.h
+//  Out of this World
+//
+//  Created by jim Veneskey on 3/26/15.
+//  Copyright (c) 2015 Jim Veneskey. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OWSpaceObject : NSObject
+
+@end
---
+++ OWSpaceObject.m
@@ -0,0 +1,13 @@
+//
+//  OWSpaceObject.m
+//  Out of this World
+//
+//  Created by jim Veneskey on 3/26/15.
+//  Copyright (c) 2015 Jim Veneskey. All rights reserved.
+//
+
+#import "OWSpaceObject.h"
+
+@implementation OWSpaceObject
+
+@end
