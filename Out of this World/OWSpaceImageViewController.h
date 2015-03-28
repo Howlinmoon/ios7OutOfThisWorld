410cbd9b28d4d64bf9629e572fcac96dedc1b3e4
Jim Veneskey <github@strifecrafter.com>
2015-03-28 11:11:57 -0400

    still hooking up the second view controller

---
+++ Out of this World/OWSpaceImageViewController.h
@@ -0,0 +1,15 @@
+//
+//  OWSpaceImageViewController.h
+//  Out of this World
+//
+//  Created by Jim Veneskey on 3/28/15.
+//  Copyright (c) 2015 Jim Veneskey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OWSpaceImageViewController : UIViewController
+
+@property (strong, nonatomic) IBOutlet UIScrollView *scrollView;
+
+@end
